d9c1f9ed1485effa011e5d86a57cb40ebd485ab9
Geanina <Geanina@DESKTOP-LJTDVOU>
2020-03-17 19:33:14 +0200

    Am creat o interfata si am testat clasa creata

--- Tema2CTS/src/ro/ase/acs/csie/clase/TestGit.java
+++ Tema2CTS/src/ro/ase/acs/csie/clase/TestGit.java
@@ -1,9 +1,14 @@
 package ro.ase.acs.csie.clase;
 
+import ro.ase.acs.csie.clase.Utilizator;
+
 public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git! Aplicatie mobila pentru planificarea activitatilor personale");
+		Utilizator u1=new Utilizator("Alexe Geanina ","alexegeanina17@stud.ase.ro",1998,1998);
+	    System.out.println("Utilizatorul are "+u1.calculVarsta()+" ani");
+	    
 
 	}
 
---
+++ Tema2CTS/src/ro/ase/acs/csie/interfete/Cont.java
@@ -0,0 +1,5 @@
+package ro.ase.acs.csie.interfete;
+
+public interface Cont {
+	public int calculVarsta();
+}

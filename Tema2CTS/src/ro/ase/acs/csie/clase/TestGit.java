24079c329524c91c682f21341b976c7825f4cf19
Geanina <Geanina@DESKTOP-LJTDVOU>
2020-03-17 19:30:29 +0200

    Este afisata denumirea lucrarii de licenta

---
+++ Tema2CTS/src/ro/ase/acs/csie/clase/TestGit.java
@@ -0,0 +1,10 @@
+package ro.ase.acs.csie.clase;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git! Aplicatie mobila pentru planificarea activitatilor personale");
+
+	}
+
+}
